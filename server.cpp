ffbcd2e0c60d873f4636bef347f957fd4fc3aa45
iTzPsychozTi <krichardson3@mail.bradley.edu>
2022-04-17 02:38:25 -0500

    Weekly Contribution

--- server.cpp
+++ server.cpp
@@ -64,32 +64,102 @@ int main()
         }
 
         /* START: game code */
-
+        
         int games = 0;
         int stayWins = 0;
         int switchWins = 0;
         int winningDoor;
 
-        //chooses random number that's either 1, 2, or 3
-        srand (time(NULL));
-        winningDoor = rand() % 3 + 1;
-        
-        allDoors();
-        cout << "Select a door \n";
-        
-        // makes user select 1 of the 3 doors
-        int selection;
-        cin >> selection;
+        cout << "Welcome to the WAP show! The way the game works is pretty simple, so don't fret. 
+                 You'll be presented three doors, two of which will contain goats and one will contain a
+                 brand new car! Pick any door you feel that the car is behind. We will then reveal the door that has the goat.
+                 You'll then be asked if you would like to choose the unreaveled door or keep the door you originally
+                 picked. Once you lock in your answer, we will then reveal what's behind the two doors. You'll either get
+                 a goat or a new car. Feeling lucky? Will you end up choosing the right door? Alright, let's play!";
 
-        // TODO add if else statements to compare their selection and then
-        // call the corresponding door method that displays the corresponding ASCII art
-
-        // TODO a do while isn't necessary but we need the logic for 3 rounds of the game to be
-        // executed with different random winning doors each round
-        do
+        //start of the actual game
+        while(games < 4)
         {
-            
-        } while (games < 3);
+            //chooses random number that's either 1, 2, or 3
+            srand (time(NULL));
+
+            winningDoor = rand() % 3 + 1;
+        
+            allDoors();
+            cout << "Select a door \n";
+        
+            // makes user select 1 of the 3 doors
+            int selection;
+            cin >> selection;
+
+            //user input for corresponding doors
+            while(selection != 1 && selection != 2 && selection != 3)
+            {
+                if(selection == 1)
+                {
+                    door1();
+                }
+                else if(selection == 2)
+                {
+                    door2();
+                }
+                else if(selection == 3)
+                {
+                    door3();
+                }
+                else
+                {
+                    cout << "Please select door 1, 2, or 3." << endl;
+                }
+            }
+
+            //TODO: game logic for choosing the goat door after user selection
+            cout << "You've chosen door " << selection << ". Let's see what's behind one of the other doors!" << endl;
+
+            //door 1 selection if statements 
+            if(winningDoor == 2 && selection == 1)
+            {
+                cout << "Well would you look at that. Door #3 had a goat behind it. You're lucky you didn't 
+                        pick that one, am I right?" << endl;
+            }
+            else if(winningDoor == 3 && selection ==1)
+            {
+                cout << "Well would you look at that. Door #2 had a goat behind it. You're lucky you didn't 
+                        pick that one, am I right?" << endl;
+            }
+        
+            //door 2 selection if satements
+            else if(winningDoor == 1 && selection == 2)
+            {
+                cout << "Well would you look at that. Door #3 had a goat behind it. You're lucky you didn't 
+                        pick that one, am I right?" << endl;
+            }
+            else if(winningDoor == 3 && selection == 2)
+            {
+                cout << "Well would you look at that. Door #1 had a goat behind it. You're lucky you didn't 
+                        pick that one, am I right?" << endl;
+            }
+
+            //door 3 selection if statements
+            else if(winningDoor == 1 && selection == 3)
+            {
+                cout << "Well would you look at that. Door #2 had a goat behind it. You're lucky you didn't 
+                        pick that one, am I right?" << endl;
+            }
+            else if(winningDoor == 2 && selection == 3)
+            {
+                cout << "Well would you look at that. Door #1 had a goat behind it. You're lucky you didn't 
+                        pick that one, am I right?" << endl;
+            }
+
+            cout << "Now that we have revealed the door with the goat behind it, you have a tough decision to make.
+                     There's a 50/50 chance you could get the door with the car behind it. But the question is...
+                     would you like to keep the door you've chosen or test your luck with the unopened door?/n 
+                     (Type '1' to keep your chosen door)/n
+                     (Type '2' to choose the unopened door";
+
+            games++;
+        } //end of games
 
         cout << "\n Out of 3, the contestant won " << stayWins << " times by staying with his/her original choice and won " << switchWins << " times by switching his/her choice.\n";
         
